6731c6f109ee71ec1fd3df1da10e2b9438f5ca45
IanWanzeele <ian.wanzeele00@gmail.com>
2024-03-12 14:21:40 +0100

    Not a list, but collection

--- ExamenNicIan/Controllers/RestaurantsController.cs
+++ ExamenNicIan/Controllers/RestaurantsController.cs
@@ -11,28 +11,25 @@ namespace ExamenNicIan.Controllers
         {
             // Make API call to retrieve restaurant data based on latitude and longitude
             var restaurants = await GetRestaurantsFromApi(model.Latitude, model.Longitude);
-            if (restaurants != null)
-            {
-                return RedirectToAction("Index", "Restaurants",restaurants);
-            }
+            var restaurantJson = JsonConvert.SerializeObject(restaurants);
+            
 
             // Pass the list of restaurants to the view
-            return View(restaurants);
+            return RedirectToAction("Index", "Map", new {restaurantJson});
         }
 
-        private async Task<List<Restaurant>> GetRestaurantsFromApi(double latitude, double longitude)
+        private async Task<Restaurant> GetRestaurantsFromApi(double latitude, double longitude)
         {
 
-            // Initialize the HttpClient
             
+
             using (var httpClient = new HttpClient())
             {
                 try
                 {
                     // Construct the URL for your API request
-                    //var apiUrl = $"https://overpass-api.de/api/interpreter?data=[out:json];node[\"amenity\"=\"restaurant\"](around:10000,{latitude},{longitude});out;";
                     var apiUrl =
-                        $"https://overpass-api.de/api/interpreter?data=[out:json];node[\"amenity\"=\"restaurant\"](around:10000,{latitude.ToString().Replace(',','.')},{longitude.ToString().Replace(',', '.')});out;";
+                        $"https://overpass-api.de/api/interpreter?data=[out:json];node[\"amenity\"=\"restaurant\"](around:10000,{latitude.ToString().Replace(',', '.')},{longitude.ToString().Replace(',', '.')});out;";
 
                     // Make the GET request to the API
                     var response = await httpClient.GetAsync(apiUrl);
@@ -43,38 +40,27 @@ namespace ExamenNicIan.Controllers
                         // Read the response content as a string
                         var responseBody = await response.Content.ReadAsStringAsync();
 
-                        // Deserialize the JSON response into a list of Restaurant objects
+                        // Deserialize the JSON response into a single Restaurant object
                         var restaurant = JsonConvert.DeserializeObject<Restaurant>(responseBody);
-                        if (restaurant != null)
-                        {
-                            var restaurants = new List<Restaurant>();
-                            restaurants.Add(restaurant);
 
-                            // Return the list of restaurants
-                            return restaurants;
-                        }
-                        else
-                        {
-                            return new List<Restaurant>(); 
-                        }
+                        // Add the single restaurant to the list
+                        return restaurant;
                     }
                     else
                     {
                         // If the request was not successful, log or handle the error accordingly
-                        // For example:
                         Console.WriteLine($"Error: {response.StatusCode} - {response.ReasonPhrase}");
                     }
                 }
                 catch (Exception ex)
                 {
                     // Handle any exceptions that may occur during the API call
-                    // For example:
                     Console.WriteLine($"An error occurred: {ex.Message}");
                 }
             }
 
             // If an error occurred or the API call failed, return an empty list
-            return new List<Restaurant>();
+            return null;
         }
 
     }
