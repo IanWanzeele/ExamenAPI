c39c07629139f59ed5b180edc5c0bb98eafc7430
IanWanzeele <ian.wanzeele00@gmail.com>
2024-03-19 14:29:16 +0100

    Functional searchbar added to Master

---
+++ ExamenNicIan/Models/GeoCodingResponse.cs
@@ -0,0 +1,25 @@
+﻿public class GeoLocationResponse
+{
+    public List<GeoLocationResult> Results { get; set; }
+}
+
+public class GeoLocationResult
+{
+    public Address Address { get; set; }
+    public Position Position { get; set; }
+}
+
+public class Address
+{
+    public string Municipality { get; set; }
+    public string CountryCode { get; set; }
+    public string Country { get; set; }
+    public string PostalCode { get; set; }
+    public string FreeformAddress { get; set; }
+}
+
+public class Position
+{
+    public double Lat { get; set; }
+    public double Lon { get; set; }
+}
--- ExamenNicIan/Program.cs
+++ ExamenNicIan/Program.cs
@@ -4,7 +4,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddHttpClient();
 builder.Services.AddScoped<RestaurantService>();
+builder.Services.AddScoped<GeoCodingService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
