daf2843934ac8b92f2c074bba3db0a594043a295
IanWanzeele <ian.wanzeele00@gmail.com>
2024-03-19 20:36:51 +0100

    Edit works

--- ExamenNicIan/Models/User.cs
+++ ExamenNicIan/Models/User.cs
@@ -1,11 +1,18 @@
-﻿namespace ExamenNicIan.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExamenNicIan.Models
 {
     public class User
     {
         public int UserId { get; set; }
+        [DisplayName("First name")]
         public string FirstName { get; set; }
+        [DisplayName("Last name")]
         public string LastName { get; set; }
+        [EmailAddress]
         public string Email { get; set; }
+        [PasswordPropertyText]
         public string Password { get; set; }
     }
 }
