2914f26e167af637d4f55242254851d26a61dae6
Hidetsugu666 <nicolas.goemanne@live.nl>
2024-03-22 14:14:37 +0100

    Fixed undefined data from map calls

--- ExamenNicIan/wwwroot/js/map.js
+++ ExamenNicIan/wwwroot/js/map.js
@@ -64,26 +64,22 @@ function fetchAndAddPOIs(latitude, longitude) {
                     var point = new atlas.data.Point([element.lon, element.lat]);
                     var feature = new atlas.data.Feature(point, {
                         name: element.tags.name,
-                        addrstreet: element.tags.addrstreet,
-                        addrhousenumber: element.tags.addrhousenumber,
-                        cuisine: element.tags.cuisine,
-                        phone: element.tags.phone,
-                        website: element.tags.website,
-                        addrcity: element.tags.addrcity,
-                        addrpostcode: element.tags.addrpostcode,
-                        stars: element.tags.stars,
-                        opening_hours: element.tags.opening_hours,
-                        description: element.tags.description
+                        addrstreet: element.tags['addr:street'] || '',
+                        addrhousenumber: element.tags['addr:housenumber'] || '',
+                        cuisine: element.tags.cuisine || '',
+                        phone: element.tags.phone || '',
+                        website: element.tags.website || '',
+                        addrcity: element.tags['addr:city'] || '',
+                        addrpostcode: element.tags['addr:postcode'] || '',
+                        stars: element.tags.stars || '',
+                        opening_hours: element.tags.opening_hours || '',
+                        description: element.tags.description || ''
                     });
                     dataSource.add(feature);
                 }
             });
 
-            map.layers.add(new atlas.layer.SymbolLayer(dataSource, null, {
-                iconOptions: {
-                    image: 'pin-round-orange', // Use an orange pin icon
-                    size: 0.5 // Adjust the size as needed
-                },
+            map.layers.add(new atlas.layer.SymbolLayer(dataSource, null, {             
                 textOptions: {
                     textField: ['get', 'name'], // Display the name of the restaurant
                     offset: [0, 1.2], // Adjust the offset as needed
